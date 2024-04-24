20ee4fe7ae8c3f22d7d9887a0489f1909a539689
goodv1ce <andriy.lysak.2003@gmail.com>
2024-04-24 12:24:11 +0300

    [INIT] initializing commit

---
+++ src/main/java/com/goodvice/pumbtest/PumbtestApplication.java
@@ -0,0 +1,13 @@
+package com.goodvice.pumbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumbtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumbtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodvice/pumbtest/PumbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodvice.pumbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
