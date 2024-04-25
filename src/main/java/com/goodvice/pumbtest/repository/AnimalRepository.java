46da2ff3fc907081497e9c420c3a9202170a305b
goodv1ce <andriy.lysak.2003@gmail.com>
2024-04-25 21:19:23 +0300

    [ADD] added Animal.java repository

--- src/main/java/com/goodvice/pumbtest/repository/AnimalRepository.java
+++ src/main/java/com/goodvice/pumbtest/repository/AnimalRepository.java
@@ -4,6 +4,8 @@ import com.goodvice.pumbtest.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
