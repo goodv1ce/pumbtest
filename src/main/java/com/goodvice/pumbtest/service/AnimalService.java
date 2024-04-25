5d441472f0a245c6090837c88132293920389675
goodv1ce <andriy.lysak.2003@gmail.com>
2024-04-25 21:19:51 +0300

    [ADD] implemented error handling via ResponseEntity

--- src/main/java/com/goodvice/pumbtest/service/AnimalService.java
+++ src/main/java/com/goodvice/pumbtest/service/AnimalService.java
@@ -5,73 +5,94 @@ import com.goodvice.pumbtest.mapper.AnimalFileParser;
 import com.goodvice.pumbtest.mapper.AnimalXmlParser;
 import com.goodvice.pumbtest.model.Animal;
 import com.goodvice.pumbtest.repository.AnimalRepository;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
+import com.goodvice.pumbtest.validator.AnimalParseValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class AnimalService {
     private final AnimalRepository animalRepository;
+    private final AnimalParseValidator validator;
 
-    public void addFromFile(MultipartFile file) {
-        if (file == null) {
-            //todo handle null file
-            return;
+    public ResponseEntity<?> addFromFile(MultipartFile file) {
+
+        if (file == null || file.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Error: File is null or empty. Unable to process.");
         }
 
         String contentType = file.getContentType();
 
         if (contentType == null) {
-            //todo handle null contentType
-            return;
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Error: Content type is not defined. Unable to process");
         }
 
-        AnimalFileParser fileMapper;
+        AnimalFileParser fileParser;
+
         switch (contentType) {
             case "text/csv":
-                fileMapper = new AnimalCsvParser();
+                fileParser = new AnimalCsvParser();
                 break;
             case "application/xml":
-                fileMapper = new AnimalXmlParser();
+                fileParser = new AnimalXmlParser();
                 break;
             default:
-                //todo handle unknown format send error unsupported
-                return;
+                return ResponseEntity
+                        .status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                        .body("Error: Content type is unsupported for processing.");
         }
-        List<Animal> animalList;
+
         try {
-            animalList = fileMapper.parse(file);
-
+            List<Animal> animalList = fileParser.parse(file);
+            validator.validate(animalList);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(animalRepository.saveAll(animalList));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e);
         }
 
-        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
-        Validator validator = validatorFactory.getValidator();
-        List<Animal> validatedAnimals = new ArrayList<>();
+    }
 
-        for (Animal animal : animalList) {
-            Set<ConstraintViolation<Animal>> violations = validator.validate(animal);
-            if (violations.isEmpty()) {
-                validatedAnimals.add(animal);
-            } else {
-                // Handle validation errors if needed
-                for (ConstraintViolation<Animal> violation : violations) {
-                    System.err.println("Validation error: " + violation.getMessage());
-                }
-            }
+    public ResponseEntity<?> getAllFiltered(String type,
+                                            Integer category,
+                                            String sex,
+                                            String sortBy
+    ) {
+        if (category == -1) {
+            category = null;
         }
 
-        animalRepository.saveAll(validatedAnimals);
+        Animal animal = new Animal();
+        animal.setType(type);
+        animal.setCategory(category);
+        animal.setSex(sex);
+
+        Example<Animal> example = Example.of(animal);
+        List<Animal> queryResult;
+        if (Objects.nonNull(sortBy)) {
+            queryResult = animalRepository.findAll(example, Sort.by(sortBy));
+        } else {
+            queryResult = animalRepository.findAll(example);
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(queryResult);
     }
 }
