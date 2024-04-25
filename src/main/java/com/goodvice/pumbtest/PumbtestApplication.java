e88e2e93a25a90dbb63905379d309bd3396100b6
goodv1ce <andriy.lysak.2003@gmail.com>
2024-04-25 21:22:08 +0300

    [REFACTOR] code refactoring

--- src/main/java/com/goodvice/pumbtest/PumbtestApplication.java
+++ src/main/java/com/goodvice/pumbtest/PumbtestApplication.java
@@ -2,8 +2,6 @@ package com.goodvice.pumbtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-
 @SpringBootApplication
 public class PumbtestApplication {
 
--- src/main/java/com/goodvice/pumbtest/repository/AnimalRepository.java
+++ src/main/java/com/goodvice/pumbtest/repository/AnimalRepository.java
@@ -4,8 +4,6 @@ import com.goodvice.pumbtest.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
 }
